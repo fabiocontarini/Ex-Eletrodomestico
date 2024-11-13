1df7b35661e200ba195fd1262ca03f4279487054
fabiocontarini <fabio.contarini@aluno.senai.br>
2024-11-13 09:06:26 -0300

    Adicionar arquivos de projeto.

---
+++ Ex-Eletrodomestico/Controllers/ListaEletrodomesticoController.cs
@@ -0,0 +1,21 @@
+﻿using Ex_Eletrodomestico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ex_Eletrodomestico.Controllers
+{
+    public class ListaEletrodomesticoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eletrodomestico> eletrodomesticos = new List<Eletrodomestico>
+{
+                new Geladeira {Marca = "Brastemp", Modelo = "Brastemp Ative!", ConsumoEnergetico =  0.74, CapacidadeLitros = 375, TemFreezer = true },
+                new Televisor {Marca = "Samsung", Modelo = " Samsung QLED 4K - Q60C", ConsumoEnergetico =90-130 , EhSmart = true, TamanhoTela = 65},
+                new Televisor {Marca = "LG", Modelo = "75P7", ConsumoEnergetico = 15, EhSmart = true, TamanhoTela = 75 },
+
+};
+
+            return View(eletrodomesticos);
+        }
+    }
+}
